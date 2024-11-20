0c811b55291d37760175c44b4ec4e808727bedc0
rew <rew@tencorvids.com>
2024-11-20 13:38:39 -0500

    Navigate panes with <CTRL> hjkl

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,3 +8,9 @@ vim.keymap.set("n", "N", "Nzzzv")
 -- Remap jk / kj to <ESC> to exit insert mode
 vim.api.nvim_set_keymap("i", "jk", "<ESC>", { noremap = true })
 vim.api.nvim_set_keymap("i", "kj", "<ESC>", { noremap = true })
+
+-- Navigate splits with <CTRL> hjkl
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
