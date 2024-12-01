85ee2bb9b025e26a25931073af0c53706cba1080
rew <rew@tencorvids.com>
2024-12-01 17:02:33 -0500

    Obsidian: Change frontmatter generation

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -60,6 +60,19 @@ return {
 			ui = {
 				enable = false,
 			},
+
+			---@return table
+			note_frontmatter_func = function(note)
+				local out = { tags = note.tags }
+
+				if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+					for k, v in pairs(note.metadata) do
+						out[k] = v
+					end
+				end
+
+				return out
+			end,
 		},
 	},
 }
